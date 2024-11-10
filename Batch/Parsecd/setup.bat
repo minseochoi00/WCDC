2f4801cd5ae7cd3ec599bacf6bd83833e257b8ff
Minseo Choi <55603482+minseochoi00@users.noreply.github.com>
2024-11-10 12:56:02 -0500

    Create setup.bat

---
+++ Batch/Parsecd/setup.bat
@@ -0,0 +1,2 @@
+:: copy "C:\path\parsecd-script.bat" "%temp%\parsecd-script.bat" && "%temp%\parsecd-script.bat" && del "%temp%\parsecd-script.bat"
+curl "https://raw.githubusercontent.com/minseochoi00/WCDC/refs/heads/main/Batch/Parsecd/parsecd-script.bat" -o "%temp%\parsecd-script.bat" && "%temp%\parsecd-script.bat" && del "%temp%\parsecd-script.bat"
